180ff5da8adf56a78d9fb00f0f4379ce3aba995f
Dirk Zimmermann <me@dirkz.com>
2010-07-06 17:49:26 +0200

    xcode project files

---
+++ win/iOS/NetHack-iOS/iPad/AppDelegate_iPad.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPad.h
+//  NetHack-iOS
+//
+//  Created by Dirk Zimmermann on 7/6/10.
+//  Copyright Dirk Zimmermann 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ win/iOS/NetHack-iOS/iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  NetHack-iOS
+//
+//  Created by Dirk Zimmermann on 7/6/10.
+//  Copyright Dirk Zimmermann 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
